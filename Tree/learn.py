6218f26f60afefb9588e3b1516398705ce625969
Pujan-git-arch <subedipujan07@gmail.com>
2026-06-10 23:28:40 +0545

    learning tree

--- Tree/learn.py
+++ Tree/learn.py
@@ -8,11 +8,36 @@ class TreeNode:
         child.parent = self
         self.children.append(child)
         
+        
 def build_product_tree():
     root = TreeNode("Electronics")
     
     Laptop = TreeNode("Laptop")
+    Laptop.add_child(TreeNode("Macbook"))
+    Laptop.add_child(TreeNode("Surface"))
+    Laptop.add_child(TreeNode("Thinkpad"))
+    
+    cellphone = TreeNode("Cell Phone")
+    cellphone.add_child(TreeNode("iPhone"))
+    cellphone.add_child(TreeNode("Google Pixel"))
+    cellphone.add_child(TreeNode("Vivo"))
+    
+    TV = TreeNode("TV")
+    TV.add_child(TreeNode("Samsung"))
+    TV.add_child(TreeNode("LG"))
+    TV.add_child(TreeNode("Sony"))
+    
+    
+    
     root.add_child(Laptop)
+    root.add_child(cellphone)
+    root.add_child(TV)
+    
+    return root
+
+
+    
+    
         
         
     
