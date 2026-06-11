0b5553d77d7da1f8ce13fb59f022f6b0ec74e312
Pujan-git-arch <subedipujan07@gmail.com>
2026-06-11 14:37:52 +0545

    learnt tree

--- Tree/learn.py
+++ Tree/learn.py
@@ -8,7 +8,22 @@ class TreeNode:
         child.parent = self
         self.children.append(child)
         
-        
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+        return level
+
+    def print_tree(self):
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else ""
+        print(prefix +self.data)
+        if len(self.children) > 0:
+            for child in self.children:
+                child.print_tree()
+
 def build_product_tree():
     root = TreeNode("Electronics")
     
@@ -34,8 +49,10 @@ def build_product_tree():
     root.add_child(TV)
     
     return root
-
-
+if __name__ == '__main__':
+    root = build_product_tree()
+    root.print_tree()
+    
     
     
         
