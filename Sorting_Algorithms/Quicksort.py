b7c25e965adffdd2c2cf199e001dc8a793dc7e66
Pujan-git-arch <subedipujan07@gmail.com>
2026-06-19 22:34:21 +0545

    Quick sort learned

--- Sorting_Algorithms/Quicksort.py
+++ Sorting_Algorithms/Quicksort.py
@@ -6,47 +6,37 @@ def swap(a, b, arr):
         arr[a] = arr[b]
         arr[b] = tmp
 
-def quick_sort(elements, start, end):
-    if start < end:
-        pi = partition(elements, start, end)
-        quick_sort(elements, start, pi-1)
-        quick_sort(elements, pi+1, end)
-
-def partition(elements, start, end):
-    pivot_index = start
-    pivot = elements[pivot_index]
-
-    while start < end:
-        while start < len(elements) and elements[start] <= pivot:
+def partition(start,end,elements):
+    p_index = start
+    pivot = elements[p_index]
+    while start< end:
+        
+        while start <= end and elements[start] <= pivot:
             start+=1
-
+        
         while elements[end] > pivot:
             end-=1
-
+            
         if start < end:
-            swap(start, end, elements)
-
-    swap(pivot_index, end, elements)
-
+            swap(start,end, elements)
+            
+    swap(p_index, end, elements)
+    
     return end
+
+def quicksort(start,end, elements):
+    if start < end:
+        pi = partition(start, end, elements)
+        quicksort(start,pi-1, elements)
+        quicksort(pi+1,end,elements)
+        
+        
+    
+    
+
 
 
 if __name__ == '__main__':
-    elements = [11,9,29,7,2,15,28]
-    # elements = ["mona", "dhaval", "aamir", "tina", "chang"]
-    quick_sort(elements, 0, len(elements)-1)
+    elements = [11, 9, 29, 7, 2, 15, 28]
+    quicksort(0,len(elements)-1,elements)
     print(elements)
-
-    tests = [
-        [11,9,29,7,2,15,28],
-        [3, 7, 9, 11],
-        [25, 22, 21, 10],
-        [29, 15, 28],
-        [],
-        [6]
-    ]
-
-    for elements in tests:
-        quick_sort(elements, 0, len(elements)-1)
-        print(f'sorted array: {elements}')
-
